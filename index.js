5aa13733907ddeeaf455e6c47b6d73bc816c9fec
Vitaliy Mikeshyn <v.mikeshyn@sevensenders.com>
2022-02-09 12:18:21 +0100

    modify to JS

--- index.js
+++ index.js
@@ -1,40 +1,90 @@
-const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require("path");
+const mime = require("mime-types");
 const core = require('@actions/core');
+const AWS = require('aws-sdk');
+const cloudfront = new AWS.CloudFront();
+const s3 = new AWS.S3();
 
-function deploy_to_cloudfront(s3_name, build_folder_path) {
-    let distrib_id;
-    try {
-        // execSync("aws configure set preview.cloudfront true");
-        const distrib_id_request = 'aws cloudfront list-distributions' +
-            ' --output text --query "DistributionList.Items[?Origins.Items[0].DomainName==\'' +
-            s3_name + '.s3.eu-central-1.amazonaws.com\'].Id"'
-        distrib_id = execSync(distrib_id_request).toString().replace(/\r?\n|\r/g, "");
-        console.log("The distribution was found.");
-    } catch (error) {
-        console.log("Failed to find the distribution for this bucket.");
-        process.exit(1);
+async function get_distribution_id(s3_name) {
+    const DistributionList = await cloudfront.listDistributions().promise();
+    for (let distribution of DistributionList.Items) {
+        if (distribution.Aliases.Items.includes(s3_name)) {
+            console.log("The distribution was found.");
+            return distribution['Id'];
+        }
     }
-    try {
-        const sync_request = 'aws s3 sync ' + build_folder_path + ' s3://' +
-            s3_name + '/'
-        execSync(sync_request);
-        console.log("Files were synced.");
-    } catch (error) {
-        console.log("Failed to sync the files to a bucket.");
-        process.exit(1);
+    console.log("Failed to find the distribution for this bucket.");
+    process.exit(1);
+}
+
+async function invalidation(distribution_ID, paths) {
+    const params = {
+        DistributionId: distribution_ID.toString(),
+        InvalidationBatch: {
+            CallerReference: `${+new Date()}`,
+            Paths: {
+                Quantity: paths.length,
+                Items: paths
+            }
+        }
     }
-    try {
-        const invalidation_request = 'aws cloudfront' +
-            ' create-invalidation --distribution-id ' +
-            distrib_id + ' --paths "/*"'
-        execSync(invalidation_request);
-        console.log("Cache was invalidated.");
-    } catch (error) {
-        console.log("Failed to invalidate cache.");
-        process.exit(1);
+    const invalidation_request = await cloudfront.createInvalidation(params).promise();
+    const wait_params = {
+        DistributionId: distribution_ID.toString(),
+        Id: invalidation_request['Invalidation']['Id'].toString()
+    };
+    cloudfront.waitFor('invalidationCompleted', wait_params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            process.exit(1);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+function upload_to_s3(build_folder_path, s3_name) {
+    const dirName = path.basename(build_folder_path);
+    function walkSync(currentDirPath, callback) {
+        fs.readdirSync(currentDirPath).forEach(function (name) {
+            const filePath = path.join(currentDirPath, name);
+            const stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                callback(filePath, stat);
+            } else if (stat.isDirectory()) {
+                walkSync(filePath, callback);
+            }
+        });
     }
+    walkSync(build_folder_path, function (filePath) {
+        if (filePath.endsWith("/")) {
+            return;
+        }
+
+        const bucketPath = `${dirName}${filePath.split(dirName)[1]}`;
+        const params = {
+            Bucket: s3_name,
+            Key: bucketPath,
+            Body: fs.readFileSync(filePath),
+            ContentType: mime.lookup(bucketPath) || "text/plain",
+        };
+
+        const uploadLog = bucketPath + " Type:" + params.ContentType;
+
+        s3.putObject(params, function (err) {
+            if (err) {
+                console.error(err.message);
+            } else {
+                console.info(`Uploaded: ${uploadLog}`);
+            }
+        });
+    });
 }
 
 const s3_name = core.getInput('s3-bucket-name');
 const build_folder_path = core.getInput('build-folder-path');
-deploy_to_cloudfront(s3_name, build_folder_path)
+upload_to_s3(s3_name, build_folder_path)
+get_distribution_id(s3_name).then(result => {
+    invalidation(result, ['/*']).then()
+})
